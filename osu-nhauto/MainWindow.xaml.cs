7e1f3b2e7f09d50b3a654a68b2fe8b9b8037dc90
Vincent Boc <japanxpanda@yahoo.com>
2018-10-24 17:29:56 -0700

    Added status window

--- osu-nhauto/MainWindow.xaml.cs
+++ osu-nhauto/MainWindow.xaml.cs
@@ -20,9 +20,63 @@ namespace osu_nhauto
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        internal static MainWindow Main;
+
+        class StatusHandler {
+
+
+            public StatusHandler()
+            {
+                autopilotRunning = false;
+                relaxRunning = false;
+            }
+
+            public void updateWindow()
+            {
+            
+                Main.statusWindow.Inlines.Add(new Run("AutoPilot: ") { FontWeight = FontWeights.Bold });
+                if (autopilotRunning)
+                {
+                    Main.statusWindow.Inlines.Add(new Run("Running") { Foreground = Brushes.Green });
+                }
+                else
+                {
+                    Main.statusWindow.Inlines.Add(new Run("Not Running") { Foreground = Brushes.Red });
+                }
+                Main.statusWindow.Inlines.Add("\n");
+                Main.statusWindow.Inlines.Add(new Run("Relax: ") { FontWeight = FontWeights.Bold });
+                if (relaxRunning)
+                {
+                    Main.statusWindow.Inlines.Add(new Run("Running") { Foreground = Brushes.Green });
+                }
+                else
+                {
+                    Main.statusWindow.Inlines.Add(new Run("Not Running") { Foreground = Brushes.Red });
+                }
+            }
+
+            public bool isAutoPilotRunning() { return this.autopilotRunning; }
+            public bool isRelaxRunning(){ return this.relaxRunning; }    
+            private bool autopilotRunning;
+            private bool relaxRunning;
+        }
+
+        public void main()
+        {
+            // status is name of status window text block
+            StatusHandler status = new StatusHandler();
+
+            status.updateWindow();
+
+        }
+
         public MainWindow()
         {
+            Main = this;
             InitializeComponent();
+
+            main();
         }
     }
 }
