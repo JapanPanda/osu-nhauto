90ecb61c4b5ac60959c8a05f19e73972c0df5e16
Vincent Boc <japanxpanda@yahoo.com>
2018-11-13 18:48:54 -0800

    Removed deprecated struct

--- osu-nhauto/HitObjects/HitObjectSliderBezier.cs
+++ osu-nhauto/HitObjects/HitObjectSliderBezier.cs
@@ -6,19 +6,7 @@ namespace osu_nhauto.HitObjects
 {
     public class HitObjectSliderBezier : HitObjectSlider
     {
-
-        private struct FPOINT
-        {
-            public float X;
-            public float Y;
-
-            public FPOINT(float x, float y)
-            {
-                this.X = x;
-                this.Y = y;
-            }
-        }
-
+        
         public HitObjectSliderBezier(osu_database_reader.Components.HitObjects.HitObjectSlider hollyObj, float sliderVelocity,
             System.Collections.Generic.List<osu_database_reader.Components.Beatmaps.TimingPoint> timingPoints) : base(hollyObj, sliderVelocity, timingPoints)
         {
